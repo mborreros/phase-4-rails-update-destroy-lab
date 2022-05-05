d5f0856c7db4d46407572d787c29a18832470b04
mborreros <mlbsindia@gmail.com>
2022-05-05 15:29:02 -0400

    commit tree 97c80aa79d3475264660c5060c280a4317f99b0c

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +18,37 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # patch /plants/:id
+  def update
+    plant = find_plant
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: {error: "plant not fount and is not able to be updated"}, status: :not_found
+    end
+  end
+
+  # delete /plants/:id
+  def destroy
+    plant = find_plant
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "plant not fount and is not able to be deleted"}, status: :not_found
+    end
+
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    plant = Plant.find_by(id: params[:id])
+  end
+
 end
